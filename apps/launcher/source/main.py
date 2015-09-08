ed7f322cd467f00ccb54b78c099944a197390305
Robert MacGregor <ragoradev@gmail.com>
2015-09-08 11:20:16 -0400

    Added typenames to the launcher

--- apps/launcher/source/main.py
+++ apps/launcher/source/main.py
@@ -9,6 +9,8 @@ import subprocess
 class Application(object):
     type_pattern = re.compile("\(\S+\)")
     console_pattern = re.compile("\(\S+\) \S+")
+    component_pattern = re.compile("\S+:")
+    
     process = None
     
     def main(self):
@@ -23,9 +25,14 @@ class Application(object):
                 
                 if (re.match(self.console_pattern, stdout_line)):
                     message_type = re.search(self.type_pattern, stdout_line).group(0).strip("()")
-                    message_content = re.split(self.console_pattern, stdout_line)[1].lstrip()
-  
-                    info_log.write("(%s at %s): %s\n" % (message_type, datetime.now(), message_content))
+                    message_content = re.split(self.type_pattern, stdout_line)[1].lstrip()
+                    
+                    if (re.match(self.component_pattern, message_content)):
+                        component_name = re.search(self.component_pattern, message_content).group(0).rstrip(":")
+                        message_content = re.split(self.component_pattern, message_content)[1].lstrip()
+                        info_log.write("(%s at %s from %s): %s\n" % (message_type, datetime.now(), component_name, message_content))
+                    else:
+                        info_log.write("(%s at %s): %s\n" % (message_type, datetime.now(), message_content))
                     
             error_log.close()
             info_log.close()
