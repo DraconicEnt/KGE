38d7fe4341110b450a2b28f0e23f3ca158992985
Robert MacGregor <ragoradev@gmail.com>
2016-06-14 18:19:51 -0400

    Whitespace adjustments in common.hpp

--- components/support/include/support/common.hpp
+++ components/support/include/support/common.hpp
@@ -34,10 +34,19 @@ namespace Kiaro
     //! An enumeration used to store versioning information.
     enum VERSION
     {
+        //! Major version of the engine.
         MAJOR = 0,
+
+        //! Minor version of the engine.
         MINOR = 0,
+
+        //! Build number of the engine.
         BUILD = 0,
+
+        //! Revision number of the engine.
         REVISION = 0,
+
+        //! Network protocol version the engine is using.
         PROTOCOL = 0
     };
 
@@ -46,72 +55,75 @@ namespace Kiaro
     {
         //! Good exit.
         EXIT_STATUS_GOOD = 0,
+
         //! Input error.
         EXIT_STATUS_INPUT = 1,
+
         //! Memory allocation error.
         EXIT_STATUS_MEMALLOC = 2,
+
         //! Engine exited via Kill()
         EXIT_STATUS_KILLED = 3,
     };
 
     namespace Common
     {
-#define _ENGINE_USE_IRRLICHT_TYPES_ 1
-#define _ENGINE_USE_GLOBAL_EXCEPTION_CATCH_ 0
+        #define _ENGINE_USE_IRRLICHT_TYPES_ 1
+        #define _ENGINE_USE_GLOBAL_EXCEPTION_CATCH_ 0
 
-#define ENGINE_ASSERT_TRUE(x) assert(x)
+        #define ENGINE_ASSERT_TRUE(x) assert(x)
 
-#if defined(_MSC_VER) || (__BORLANDC__ >= 0x530) && !defined(__STRICT_ANSI__)
-        //#define NOTHROW __declspec(nothrow)
-#define NOTHROW
-#else
-#define NOTHROW __attribute__((nothrow))
-#endif
+        #if defined(_MSC_VER) || (__BORLANDC__ >= 0x530) && !defined(__STRICT_ANSI__)
+                //#define NOTHROW __declspec(nothrow)
+                #define NOTHROW
+        #else
+            #define NOTHROW __attribute__((nothrow))
+        #endif
 
-#define NOEXCEPT
-#define CONSTEXPR
+        #define NOEXCEPT
+        #define CONSTEXPR
 
-#ifndef _ENGINE_USE_IRRLICHT_TYPES_
-#if defined(_MSC_VER) || (__BORLANDC__ >= 0x530) && !defined(__STRICT_ANSI__)
-        typedef signed __int8 S8;
-        typedef unsigned __int8 U8;
-        typedef unsigned __int16 U16;
-        typedef signed __int16 S16;
-        typedef signed __int32 S32;
-        typedef unsigned __int32 U32;
-#else
-        typedef signed char S8;
-        typedef unsigned char U8;
-        typedef unsigned short U16;
-        typedef signed short S16;
-        typedef signed int S32;
-        typedef unsigned int U32;
-#endif
+        #ifndef _ENGINE_USE_IRRLICHT_TYPES_
+            #if defined(_MSC_VER) || (__BORLANDC__ >= 0x530) && !defined(__STRICT_ANSI__)
+                    typedef signed __int8 S8;
+                    typedef unsigned __int8 U8;
+                    typedef unsigned __int16 U16;
+                    typedef signed __int16 S16;
+                    typedef signed __int32 S32;
+                    typedef unsigned __int32 U32;
+            #else
+                    typedef signed char S8;
+                    typedef unsigned char U8;
+                    typedef unsigned short U16;
+                    typedef signed short S16;
+                    typedef signed int S32;
+                    typedef unsigned int U32;
+            #endif
 
-        //! 32-bit Signed Float typedef.
-        typedef float F32;
-        //! 64-bit Signed Float typedef.
-        typedef double F64;
-        //! Signed char typedef.
-        typedef char C8;
-        //! 64-bit integer typedef.
-        typedef unsigned long long int U64;
-#else
-        typedef irr::s8 S8;
-        typedef irr::u8 U8;
-        typedef irr::u16 U16;
-        typedef irr::s16 S16;
-        typedef irr::s32 S32;
-        typedef irr::u32 U32;
+            //! 32-bit Signed Float typedef.
+            typedef float F32;
+            //! 64-bit Signed Float typedef.
+            typedef double F64;
+            //! Signed char typedef.
+            typedef char C8;
+            //! 64-bit integer typedef.
+            typedef unsigned long long int U64;
+        #else
+                typedef irr::s8 S8;
+                typedef irr::u8 U8;
+                typedef irr::u16 U16;
+                typedef irr::s16 S16;
+                typedef irr::s32 S32;
+                typedef irr::u32 U32;
 
-        typedef irr::f32 F32;
-        typedef irr::f64 F64;
-        typedef irr::c8 C8;
-        typedef irr::u64 U64;
+                typedef irr::f32 F32;
+                typedef irr::f64 F64;
+                typedef irr::c8 C8;
+                typedef irr::u64 U64;
 
-        typedef irr::core::vector3df Vector3DF;
-        typedef irr::video::SColor ColorRGBA;
-#endif
+                typedef irr::core::vector3df Vector3DF;
+                typedef irr::video::SColor ColorRGBA;
+        #endif
     } // End NameSpace Common
 } // End NameSpace Kiaro
 #endif // _INCLUDE_KIARO_ENGINE_COMMON_
