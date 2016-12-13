36cbae0ae37314c277fbed3ba47d909ef54003e1
Robert MacGregor <ragoradev@gmail.com>
2016-12-12 21:01:15 -0500

    Include a simple mainpage on the doxygen output

---
+++ doxymain.h
@@ -0,0 +1,13 @@
+/**
+ *  @file doxymain.h
+ *  @date 12/12/2016
+ *  @brief Main page file for use with Doxygen. This file does absolutely nothing otherwise.
+ *  @author <a href="http://dx.no-ip.org">Robert MacGregor</a>
+ *
+ *  @mainpage Welcome
+ *  Kiaro Game Engine is an experimental work-in-progress game engine taking some design ideology from Torque Game Engine. It is intended to
+ *  take all the good design philsophy from Torque and leaving the bad behind, replacing it with better ideologies instead.
+ *
+ *  This is the technical documentation for all the various system internals in their current implementation state.
+ *
+ */
